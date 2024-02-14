c592c6bfc96b1aa3747ca3e2cdd53fd3681d1234
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-02-15 00:10:58 +0600

    added function to remove the text after clicking

---
+++ update-value-by-click.js
@@ -0,0 +1,5 @@
+function handleOnClick() {
+  const handlerStatus = document.getElementById("handler-status");
+  handlerStatus.innerText =
+    "Handler by function attached on onclick attribute.";
+}
