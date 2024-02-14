097bb754b060be142b8cd558754544802366b4a6
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-02-15 00:53:01 +0600

    added function to remove the text after clicking but added second button

--- update-value-by-click.js
+++ update-value-by-click.js
@@ -1,5 +1,10 @@
 function handleOnClick() {
   const handlerStatus = document.getElementById("handler-status");
-  handlerStatus.innerText =
-    "Handler by function attached on onclick attribute.";
+  handlerStatus.innerText = "Handler by function attached on onclick attribute";
 }
+
+// option 2
+document.getElementById("event-listner").addEventListener("click", function () {
+  const handlerStatus = document.getElementById("handler-status");
+  handlerStatus.innerText = "Text updated by add Event listener button";
+});
