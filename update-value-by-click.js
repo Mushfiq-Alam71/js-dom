2ee3f775ddbbfe93472ca9d2c0b79538c5780eaa
Mushfiq Alam Sami <mushfiq9912@gmail.com>
2024-02-15 11:14:11 +0600

    creating function updating value of a text using input field and button

--- update-value-by-click.js
+++ update-value-by-click.js
@@ -1,3 +1,4 @@
+// option 1
 function handleOnClick() {
   const handlerStatus = document.getElementById("handler-status");
   handlerStatus.innerText = "Handler by function attached on onclick attribute";
@@ -7,4 +8,16 @@ function handleOnClick() {
 document.getElementById("event-listner").addEventListener("click", function () {
   const handlerStatus = document.getElementById("handler-status");
   handlerStatus.innerText = "Text updated by add Event listener button";
+});
+
+//update using button click
+document.getElementById("btn-update").addEventListener("click", function () {
+  const inputField = document.getElementById("input-field");
+  const inputText = inputField.value;
+
+  //setting the value
+  const p = document.getElementById("input-text-display");
+  p.innerText = inputText;
+
+  inputField.value = ""; //to set the value empty string after click
 });
